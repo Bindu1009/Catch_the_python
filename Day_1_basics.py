7f1c94d27bcb900e45176befeb2f79e85cadef80
Bindu <himabindugunti2@gmail.com>
2026-05-19 16:08:45 +0000

    Add Day 1 palindrome solutions

--- Day_1_basics.py
+++ Day_1_basics.py
@@ -1,6 +1,23 @@
 # 1.Pallindrome:  Given an integer x, return True if it is a palindrome (reads same forward/backward).
 # solution 1: using string
-def isPallindrome_str(x):
+def isPallindrome_string(x):
     return str(x) == str(x)[::-1]
-print(isPallindrome_str(121))
-print(isPallindrome_str(-121))
+print(isPallindrome_string(121))
+print(isPallindrome_string(-121))
+# solution 2: without using string
+def isPallindrome(x):
+    # edge cases
+    if x < 0:
+        return False
+    if x < 10:
+        return True
+    original = x
+    reverse_num = 0
+
+    while x > 0:
+        last_digit = x % 10
+        reverse_num = reverse_num * 10 + last_digit
+        x = x//10 
+    return original == reverse_num
+print(isPallindrome(121))
+print(isPallindrome(123))
